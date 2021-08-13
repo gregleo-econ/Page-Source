d575e7fcb3da71fdedbd95b6cbed3aff161e6198
Greg <gregleo@gmail.com>
2021-08-13 11:59:50 -0500

    Trying actions

--- pageMaker.R
+++ pageMaker.R
@@ -1,7 +1,6 @@
-directory="c://webpage"
+directory="/"
 setwd(directory)
 source("background.R")
-
 style = paste(c(directory,"//style/style.css"),collapse="")
 makePage(directory,style,"Greg Leo")
 
